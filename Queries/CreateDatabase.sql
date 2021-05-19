812de1626374bbb241b44b016c77bcf777a5f66b
Vlad <Vsoilol1701@gmail.com>
2021-05-19 18:01:18 +0300

    fixed problems with db query

--- Queries/CreateDatabase.sql
+++ Queries/CreateDatabase.sql
@@ -8,22 +8,29 @@ CREATE TABLE "Ingredient"
     Weight REAL NOT NULL,
 );
 
+CREATE TABLE "Category"
+(
+    Id INT PRIMARY KEY IDENTITY,
+    Name NVARCHAR(30) NOT NULL
+);
+
 CREATE TABLE "Recipe"
 (
     Id INT PRIMARY KEY IDENTITY,
     Name NVARCHAR(20) NOT NULL,
-    CookingTime INT NOT NULL,
+    CookingTime TIME NOT NULL,
     CookingTemperature TIME NOT NULL,
-    Category NVARCHAR(30) NOT NULL,
-    ImagePath NVARCHAR(50) NOT NULL,
-	Description NVARCHAR(100) NULL,
-	SequenceActions NVARCHAR(300) NULL,
+    CategoryId INT NOT NULL,
+    ImageData VARBINARY(MAX) NOT NULL,
+	Description NVARCHAR(50) NULL,
+	SequenceActions NVARCHAR(100) NULL,
+    FOREIGN KEY (CategoryId)  REFERENCES "Category" (Id)
 );
 
 CREATE TABLE "RecipeIngredient"
 (
-	IngredientId INT NULL,
-	RecipeId INT NULL,
+	IngredientId INT NOT NULL,
+	RecipeId INT NOT NULL,
 	FOREIGN KEY (IngredientId)  REFERENCES "Ingredient" (Id),
 	FOREIGN KEY (RecipeId)  REFERENCES "Recipe" (Id)
 );
