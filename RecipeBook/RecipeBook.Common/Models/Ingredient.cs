d90f71c4d9827d0f5c3c065e5dce112419873d66
Vlad <Vsoilol1701@gmail.com>
2021-05-23 20:55:55 +0300

    Create models

---
+++ RecipeBook/RecipeBook.Common/Models/Ingredient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecipeBook.Common.Models
+{
+    public class Ingredient
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Weight { get; set; }
+    }
+}
---
+++ RecipeBook/RecipeBook.Common/Models/Recipe.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecipeBook.Common.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime CookingTime { get; set; }
+
+        public int CookingTemperature { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public byte[] ImageData { get; set; }
+
+        public string Description { get; set; }
+
+        public string SequenceActions { get; set; }
+    }
+}
