383b59733cd536aa1d044bfe996e75fb11f09636
Vlad <Vsoilol1701@gmail.com>
2021-05-23 21:19:47 +0300

    Create DBHelper and DBConfig

---
+++ RecipeBook/RecipeBook.Dal/Helpers/Implementations/DbHelper.cs
@@ -0,0 +1,61 @@
+﻿using RecipeBook.Common.Helpers;
+using RecipeBook.Dal.Helpers.Interfaces;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeBook.Dal.Helpers.Implementations
+{
+    public class DbHelper : IDbHelper
+    {
+        private readonly DbConfig config;
+
+        public DbHelper(DbConfig config)
+        {
+            this.config = config;
+        }
+
+        public async Task<SqlDataReader> ExecuteReaderAsync(SqlConnection connection, string sqlExpression)
+        {
+            connection.ConnectionString = config.ConnectionString;
+            await connection.OpenAsync();
+
+            SqlCommand command = new SqlCommand(sqlExpression, connection);
+            SqlDataReader reader = await command.ExecuteReaderAsync();
+
+            return reader;
+        }
+
+        public async Task<int> ExecuteNonQueryAsync(SqlConnection connection, string sqlExpression)
+        {
+            connection.ConnectionString = config.ConnectionString;
+            await connection.OpenAsync();
+
+            SqlCommand command = new SqlCommand(sqlExpression, connection);
+            return await command.ExecuteNonQueryAsync();
+        }
+
+        public async Task<SqlDataReader> ExecuteReaderAsync(SqlConnection connection, string sqlExpression, IEnumerable<SqlParameter> parameters)
+        {
+            connection.ConnectionString = config.ConnectionString;
+            await connection.OpenAsync();
+
+            SqlCommand command = new SqlCommand(sqlExpression, connection);
+            command.Parameters.AddRange(parameters.ToArray());
+            SqlDataReader reader = await command.ExecuteReaderAsync();
+
+            return reader;
+        }
+
+        public async Task<int> ExecuteNonQueryAsync(SqlConnection connection, string sqlExpression, IEnumerable<SqlParameter> parameters)
+        {
+            connection.ConnectionString = config.ConnectionString;
+            await connection.OpenAsync();
+
+            SqlCommand command = new SqlCommand(sqlExpression, connection);
+            command.Parameters.AddRange(parameters.ToArray());
+            return await command.ExecuteNonQueryAsync();
+        }
+    }
+}
---
+++ RecipeBook/RecipeBook.Di/Ioc.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RecipeBook.Dal.Helpers.Implementations;
+using RecipeBook.Dal.Helpers.Interfaces;
+
+namespace RecipeBook.Di
+{
+    public static class Ioc
+    {
+        public static void Build(this IServiceCollection services)
+        {
+            //Helpers
+            services.AddSingleton<IDbHelper, DbHelper>();
+        }
+    }
+}
