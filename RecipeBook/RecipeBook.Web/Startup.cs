76fff4e1fe060f21c55310a4772a9773229658b6
Vlad <Vsoilol1701@gmail.com>
2021-06-06 14:10:15 +0300

    Add development error

--- RecipeBook/RecipeBook.Web/Startup.cs
+++ RecipeBook/RecipeBook.Web/Startup.cs
@@ -39,14 +39,14 @@ namespace RecipeBook.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //if (env.IsDevelopment())
-            //{
-            //    app.UseDeveloperExceptionPage();
-            //}
-            //else
-            //{
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
                 app.UseExceptionHandler("/Error/Problem");
-            //}
+            }
 
             app.UseStaticFiles();
             app.UseCookiePolicy();
